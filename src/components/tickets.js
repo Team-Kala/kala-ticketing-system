4481a668a40c1eef991c9ee4523ec7041e22a3ec
Raymond Huang <raymondhuang@Raymonds-MacBook-Pro.local>
2022-02-23 14:15:48 -0500

    added drag and drop feature

--- src/components/tickets.js
+++ src/components/tickets.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Ticket = (props) => {
-  //const { _id, first_name, department, issue_title, issue_summary, status, priority, date } = state.props
 
-  // useEffect(() => {});
 
   const handleDelete = () => {
     console.log('THIS IS THE ID', props.ticketID)
@@ -21,34 +20,40 @@ const Ticket = (props) => {
   };
 
   return (
-    <article>
-      <div className='ticketHead'>
-        <div className='ticket-title'>
-          <h4>{props.ticket.issue_title}</h4>
+    <Draggable key={props.index} draggableId={`${props.ticketID}${props.index}`} index={props.index}>
+      {(provided) => (
+        <div ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}>
+          <article>
+            <div className='ticketHead'>
+              <div className='ticket-title'>
+                <h4>{props.ticket.issue_title}</h4>
+              </div>
+              <div className='delete-button'>
+                <button className='delete' onClick={handleDelete}>
+                  <i className='bi bi-trash'></i>
+                </button>
+              </div>
+            </div>
+            <div>
+              <ul className='ticketDetailList'>
+                <li className='ticketDetail'>Name: {props.ticket.first_name}</li>
+                <li className='ticketDetail'>
+                  Summary: {props.ticket.issue_summary}
+                </li>
+                <li className='ticketDetail'>
+                  Department: {props.ticket.department}
+                </li>
+                <li className='ticketDetail'>Priority: {props.ticket.priority}</li>
+                <li className='ticketDetail'>Status: {props.ticket.status}</li>
+                <li className='ticketDetail'>Date: {props.ticket.date}</li>
+              </ul>
+            </div>
+          </article>
         </div>
-        <div className='delete-button'>
-          {/* <button class='btn btn-outline-secondary'>🗑 </button> */}
-          <button className='delete' onClick={handleDelete}>
-            <i className='bi bi-trash'></i>
-          </button>
-        </div>
-      </div>
-      <div>
-        <ul className='ticketDetailList'>
-          {/* {console.log('PROPS:', props)} */}
-          <li className='ticketDetail'>Name: {props.ticket.first_name}</li>
-          <li className='ticketDetail'>
-            Summary: {props.ticket.issue_summary}
-          </li>
-          <li className='ticketDetail'>
-            Department: {props.ticket.department}
-          </li>
-          <li className='ticketDetail'>Priority: {props.ticket.priority}</li>
-          <li className='ticketDetail'>Status: {props.ticket.status}</li>
-          <li className='ticketDetail'>Date: {props.ticket.date}</li>
-        </ul>
-      </div>
-    </article>
+      )}
+    </Draggable>
   );
 };
 
--- src/containers/ticketsContainer.js
+++ src/containers/ticketsContainer.js
@@ -1,26 +1,148 @@
 import React, { useEffect, useState } from 'react';
 import TicketColumn from '../components/ticketColumns'
 
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const initialColumns = {
+  high: {
+    id: 'high',
+    list: []
+  },
+  med: {
+    id: 'med',
+    list: []
+  },
+  low: {
+    id: 'low',
+    list: []
+  }
+}
 const TicketContainer = (props) => {
-  // useEffect(() => {
-  //   //where fetch request will go 
-  // })
+  const [tickets, setTickets] = useState([]);
+  const [columns, setColumns] = useState(initialColumns)
 
-  //const [ ticketPriority, sortTickets ] = useState([])
 
-  // NOTE: TicketContainer contains all tickets
+  const fetchTickets = () => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((tickets) => {
+        setTickets(tickets)
+      })
+      .catch((err) => console.log('Error getting tickets.', err));
+  };
+
+  useEffect(() => {
+    fetchTickets();
+  }, []);
+
+  const highPriority = tickets.filter(
+    (ticket) => ticket.priority === 'high'
+  );
+  const mediumPriority = tickets.filter(
+    (ticket) => ticket.priority === 'medium'
+  );
+  const lowPriority = tickets.filter(
+    (ticket) => ticket.priority === 'low'
+  );
+
+  useEffect(() => {
+    setColumns({
+      high: {
+        id: 'high',
+        list: highPriority
+      },
+      med: {
+        id: 'med',
+        list: mediumPriority
+      },
+      low: {
+        id: 'low',
+        list: lowPriority
+      }})
+  }, [tickets])
+
+  const onDragEndHandler = ({ source, destination }) => {
+    // console.log('source ', source);
+    // console.log('destination ', destination);
+
+    // Make sure we have a valid destination
+    if (destination === undefined || destination === null) return null
+
+    // If the source and destination columns are the same
+    // AND if the index is the same, the item isn't moving
+    if (
+      source.droppableId === destination.droppableId &&
+      destination.index === source.index
+    )
+      return null
+
+    // Set start and end variables
+    const start = columns[source.droppableId]
+    const end = columns[destination.droppableId]
+    console.log('start ', start);
+    console.log('end ', end);
+
+    // If start is the same as end, we're in the same column
+    if (start === end) {
+      // Move the item within the list
+      // Start by making a new list without the dragged item
+      const newList = start.list.filter(
+        (_, idx) => idx !== source.index
+      )
+
+      // Then insert the item at the right location
+      newList.splice(destination.index, 0, start.list[source.index])
+
+      // Then create a new copy of the column object
+      const newCol = {
+        id: start.id,
+        list: newList
+      }
+
+      // Update the state
+      return setColumns(state => ({ ...state, [newCol.id]: newCol }))
+      // return null
+    } else {
+      // If start is different from end, we need to update multiple columns
+      // Filter the start list like before
+      const newStartList = start.list.filter(
+        (_, idx) => idx !== source.index
+      )
+
+      // Create a new start column
+      const newStartCol = {
+        id: start.id,
+        list: newStartList
+      }
+
+      // Make a new end list array
+      const newEndList = end.list
+
+      // Insert the item into the end list
+      newEndList.splice(destination.index, 0, start.list[source.index])
+
+      // Create a new end column
+      const newEndCol = {
+        id: end.id,
+        list: newEndList
+      }
+
+      // Update the state
+      return setColumns(state => ({
+        ...state,
+        [newStartCol.id]: newStartCol,
+        [newEndCol.id]: newEndCol
+      }))
+    }
+  }
   return (
-    <div className='ticket-list'>
-      <div className='columns' id='high-priority'>
-        <TicketColumn priority='high' />
+    <DragDropContext onDragEnd={onDragEndHandler}>
+      <div className='ticket-list'>
+        {Object.values(columns).map((col, index) => (
+          <TicketColumn key={index} info={col.list} dropColumn={col.id} setTickets={setTickets}/>
+        ))}
       </div>
-      <div className='columns' id='medium-priority'>
-        <TicketColumn priority='medium' />
-      </div>
-      <div className='columns' id='low-priority'>
-        <TicketColumn priority='low' />
-      </div>
-    </div>
+    </DragDropContext>
   );
 }
 
